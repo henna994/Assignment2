204037a3317732d6d5cb0c2966c6331a93ed2ce7
henna <hghayas@oakland.edu>
2019-02-09 13:17:14 -0500

    function with code

--- BeerSong.java
+++ BeerSong.java
@@ -10,9 +10,22 @@
  */
 public class BeerSong {
     public static void Ninety_Nine_Bottles_of_Beer()
-  
+   {
+        for(int i = 99; i>=1; i--){
+            int less = i -1;
+            if (i == 1){
+               System.out.println(i +" bottle of beer on the wall," +i+ " bottle of beer take one down, "
+                   + "\npass it around, no more bottles of beer on the wall.");
+            }
+               else {
+                    System.out.println(i +" bottles of beer on the wall," +i+ " bottles of beer take one down, "
+                   + "\npass it around," + less + " bottles of beer on the wall.");
+                       
+            }
+   }
+   }
     public static void main(String[] args) {
-      
+        Ninety_Nine_Bottles_of_Beer();
            }
 }
     
