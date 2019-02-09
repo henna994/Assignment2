08f6222ce7ed341d8b11d77530d6c29735eb0384
henna <hghayas@oakland.edu>
2019-02-09 13:07:24 -0500

    Just functions

---
+++ BeerSong.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author henna
+ */
+public class BeerSong {
+    public static void Ninety_Nine_Bottles_of_Beer()
+  
+    public static void main(String[] args) {
+      
+           }
+}
+    
+    
+
